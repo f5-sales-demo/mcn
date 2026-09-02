7a7bb59ba5abd85b7c65e6901bf9cf713fe0d587
Robin Mordasiewicz <robin@mordasiewicz.com>
2026-09-02 13:19:11 -0400

    fix(terraform): pin verified SMSv2 provider release (#1083)

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     xcsh = {
       source  = "f5-sales-demo/xcsh"
-      version = "= 6.0.0"
+      version = "= 6.1.2"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
