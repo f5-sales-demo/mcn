330a559add4139c5cd4a3ca43852c72145e63612
Robin Mordasiewicz <robin@mordasiewicz.com>
2026-09-17 06:39:49 -0400

    fix(terraform): tag root TGW module ownership

--- tests/test-aws-root-owned-resource-tags.sh
+++ tests/test-aws-root-owned-resource-tags.sh
@@ -21,6 +21,17 @@ require_block() {
 require_block ce "$repo_root/terraform/aws/aws_ce.tf"
 require_block workload "$repo_root/terraform/aws/aws_vpc.tf"
 
+require_tgw_module_ownership_tags() {
+  local source_file=$1
+  local block
+  block=$(sed -n '/module \"aws_tgw_connect\" {/,/^}/p' "$source_file")
+  grep -Eq '^[[:space:]]*ownership_tags[[:space:]]*=[[:space:]]*local\.tags$' <<<"$block" ||
+    fail "aws_tgw_connect in ${source_file#$repo_root/} must pass immutable ownership tags"
+}
+
+require_tgw_module_ownership_tags "$repo_root/terraform/aws/aws_tgw_connect.tf"
+require_tgw_module_ownership_tags "$repo_root/terraform/aws_tgw_connect.tf"
+
 # The tenant guard must be a dependency of planned object metadata. An unused
 # data source is not evaluated by Terraform, which would let a wrong tenant
 # reach the provider before its postcondition could reject the plan.
